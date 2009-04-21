cbb1b854111418eb99965bb7ffdef89c95800e49
Brian Lopez <seniorlopez@gmail.com>
2009-04-21 08:54:56 -0700

    more work on hash creation, almost there! (a lot more test subjects work

--- yajl.c
+++ yajl.c
@@ -2,22 +2,22 @@
 #include <yajl/yajl_gen.h>
 #include <ruby.h>
 
-VALUE currentNest;
+VALUE currentNest, context;
 
 static int found_null(void * ctx) {
     VALUE val = Qnil;
     
     switch (TYPE(currentNest)) {
         case T_HASH:
-            rb_hash_aset(currentNest, (VALUE)ctx, val);
-            ctx = (void *)currentNest;
+            rb_hash_aset(currentNest, context, val);
+            context = currentNest;
             break;
         case T_ARRAY:
             rb_ary_push(currentNest, val);
-            ctx = (void *)currentNest;
+            context = currentNest;
             break;
         default:
-            ctx = (void *)val;
+            context = val;
             break;
     }
     return 1;
@@ -28,15 +28,15 @@ static int found_boolean(void * ctx, int boolean) {
     
     switch (TYPE(currentNest)) {
         case T_HASH:
-            rb_hash_aset(currentNest, (VALUE)ctx, val);
-            ctx = currentNest;
+            rb_hash_aset(currentNest, context, val);
+            context = currentNest;
             break;
         case T_ARRAY:
             rb_ary_push(currentNest, val);
-            ctx = currentNest;
+            context = currentNest;
             break;
         default:
-            ctx = val;
+            context = val;
             break;
     }
     return 1;
@@ -47,15 +47,15 @@ static int found_integer(void * ctx, long integerVal) {
     
     switch (TYPE(currentNest)) {
         case T_HASH:
-            rb_hash_aset(currentNest, (VALUE)ctx, val);
-            ctx = (void *)currentNest;
+            rb_hash_aset(currentNest, context, val);
+            context = currentNest;
             break;
         case T_ARRAY:
             rb_ary_push(currentNest, val);
-            ctx = (void *)currentNest;
+            context = currentNest;
             break;
         default:
-            ctx = (void *)val;
+            context = val;
             break;
     }
     return 1;
@@ -66,70 +66,69 @@ static int found_double(void * ctx, double doubleVal) {
     
     switch (TYPE(currentNest)) {
         case T_HASH:
-            rb_hash_aset(currentNest, (VALUE)ctx, val);
-            ctx = (void *)currentNest;
+            rb_hash_aset(currentNest, context, val);
+            context = currentNest;
             break;
         case T_ARRAY:
             rb_ary_push(currentNest, val);
-            ctx = (void *)currentNest;
+            context = currentNest;
             break;
         default:
-            ctx = (void *)val;
+            context = val;
             break;
     }
     return 1;
 }
 
 static int found_string(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
-    VALUE val = rb_str_new(stringVal, stringLen);
+    VALUE val = rb_str_new((char *)stringVal, stringLen);
     
     switch (TYPE(currentNest)) {
         case T_HASH:
-            rb_hash_aset(currentNest, (VALUE)ctx, val);
-            ctx = (void *)currentNest;
+            rb_hash_aset(currentNest, context, val);
+            context = currentNest;
             break;
         case T_ARRAY:
             rb_ary_push(currentNest, val);
-            ctx = (void *)currentNest;
+            context = currentNest;
             break;
         default:
-            ctx = (void *)val;
+            context = val;
             break;
     }
     return 1;
 }
 
 static int found_hash_key(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
-    VALUE str = rb_str_new(stringVal, stringLen);
-    ctx = (void *)str;
+    VALUE str = rb_str_new((char *)stringVal, stringLen);
+    context = str;
     
     if (currentNest != Qnil) {
         rb_hash_aset(currentNest, str, Qnil);
-        ctx = (void *)currentNest;
     }
     return 1;
 }
 
 static int found_start_hash(void * ctx) {
     currentNest = rb_hash_new();
-    ctx = (void *)currentNest;
+    context = currentNest;
     return 1;
 }
 
 static int found_end_hash(void * ctx) {
-    ctx = (void *)currentNest;
+    context = currentNest;
     currentNest = Qnil;
     return 1;
 }
 
 static int found_start_array(void * ctx) {
     currentNest = rb_ary_new();
-    ctx = (void *)currentNest;
+    context = currentNest;
     return 1;
 }
 
 static int found_end_array(void * ctx) {
-    ctx = (void *)currentNest;
+    context = currentNest;
     currentNest = Qnil;
     return 1;
 }
@@ -156,10 +155,10 @@ static VALUE t_parse(VALUE self, VALUE io) {
     int bufferSize = 8192;
     yajl_parser_config cfg = {1, 1};
     intern_io_read = rb_intern("read");
-    VALUE ctx = Qnil;
+    // VALUE ctx = Qnil;
     
     // allocate our parser
-    hand = yajl_alloc(&callbacks, &cfg, NULL, (void *)ctx);
+    hand = yajl_alloc(&callbacks, &cfg, NULL, (void *)context);
     VALUE parsed = rb_str_new2("");
     VALUE rbufsize = INT2FIX(bufferSize);
     
@@ -167,10 +166,10 @@ static VALUE t_parse(VALUE self, VALUE io) {
     while (rb_io_eof(io) == Qfalse) {
         rb_funcall(io, intern_io_read, 2, rbufsize, parsed);
         
-        stat = yajl_parse(hand, RSTRING_PTR(parsed), RSTRING_LEN(parsed));
+        stat = yajl_parse(hand, (const unsigned char *)RSTRING_PTR(parsed), RSTRING_LEN(parsed));
         
         if (stat != yajl_status_ok && stat != yajl_status_insufficient_data) {
-            unsigned char * str = yajl_get_error(hand, 1, RSTRING_PTR(parsed), RSTRING_LEN(parsed));
+            unsigned char * str = yajl_get_error(hand, 1, (const unsigned char *)RSTRING_PTR(parsed), RSTRING_LEN(parsed));
             fprintf(stderr, (const char *) str);
             yajl_free_error(hand, str);
             break;
@@ -181,7 +180,7 @@ static VALUE t_parse(VALUE self, VALUE io) {
     stat = yajl_parse_complete(hand);
     yajl_free(hand);
     
-    return ctx;
+    return context;
 }
 
 VALUE mYajl;
