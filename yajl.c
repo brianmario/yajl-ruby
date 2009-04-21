0393b5f489d4d6d72fc869fe6d452ff0c6da20a5
Brian Lopez <seniorlopez@gmail.com>
2009-04-20 20:40:31 -0700

    more work on building the hash, removing faulty unicode json test case

--- yajl.c
+++ yajl.c
@@ -2,58 +2,69 @@
 #include <yajl/yajl_gen.h>
 #include <ruby.h>
 
-ID intern_io_read;
-
-static int parse_null(void * ctx) {
+static int found_null(void * ctx) {
     return 1;
 }
 
-static int parse_boolean(void * ctx, int boolean) {
+static int found_boolean(void * ctx, int boolean) {
     return 1;
 }
 
-static int parse_number(void * ctx, const char * s, unsigned int stringLen) {
+static int found_integer(void * ctx, long integerVal) {
     return 1;
 }
 
-static int parse_string(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
+static int found_double(void * ctx, double integerVal) {
     return 1;
 }
 
-static int parse_map_key(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
+static int found_string(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
     return 1;
 }
 
-static int parse_start_map(void * ctx) {
-    // cast with: (VALUE) ctx
+static int found_map_key(void * ctx, const unsigned char * stringVal, unsigned int stringLen) {
+    char key[stringLen];
+    sprintf(key, "%.*s", stringLen, &stringVal[0]);
+    rb_hash_aset((VALUE)ctx, rb_str_new2(key), Qnil);
     return 1;
 }
 
-static int parse_end_map(void * ctx) {
+static int found_start_map(void * ctx) {
+    if ((VALUE)ctx == Qnil) {
+       ctx = (void *)rb_hash_new();
+    } else {
+        // TODO - nested hash
+    }
     return 1;
 }
 
-static int parse_start_array(void * ctx) {
+static int found_end_map(void * ctx) {
     return 1;
 }
 
-static int parse_end_array(void * ctx) {
+static int found_start_array(void * ctx) {
+    return 1;
+}
+
+static int found_end_array(void * ctx) {
     return 1;
 }
 
 static yajl_callbacks callbacks = {
-    parse_null,
-    parse_boolean,
+    found_null,
+    found_boolean,
+    found_integer,
+    found_double,
     NULL,
-    NULL,
-    parse_number,
-    parse_string,
-    parse_start_map,
-    parse_map_key,
-    parse_end_map,
-    parse_start_array,
-    parse_end_array
+    found_string,
+    found_start_map,
+    found_map_key,
+    found_end_map,
+    found_start_array,
+    found_end_array
 };
+
+ID intern_io_read;
 
 static VALUE t_parse(VALUE self, VALUE io) {
     yajl_handle hand;
@@ -71,13 +82,11 @@ static VALUE t_parse(VALUE self, VALUE io) {
     // now parse from the IO
     while (rb_io_eof(io) == Qfalse) {
         rb_funcall(io, intern_io_read, 2, rbufsize, parsed);
-        
-        stat = yajl_parse(hand, (unsigned char const *)RSTRING_PTR(parsed), RSTRING_LEN(parsed));
+        stat = yajl_parse(hand, RSTRING_PTR(parsed), RSTRING_LEN(parsed));
         
         if (stat != yajl_status_ok &&
-            stat != yajl_status_insufficient_data)
-        {
-            unsigned char * str = yajl_get_error(hand, 1, (unsigned char const *)RSTRING_PTR(parsed), RSTRING_LEN(parsed));
+            stat != yajl_status_insufficient_data) {
+            unsigned char * str = yajl_get_error(hand, 1, RSTRING_PTR(parsed), RSTRING_LEN(parsed));
             fprintf(stderr, (const char *) str);
             yajl_free_error(hand, str);
             break;
