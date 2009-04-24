87d03fc2c323f5a5c7e308641b8e4ed7eac1d2bd
Brian Lopez <seniorlopez@gmail.com>
2009-04-24 10:37:32 -0700

    refactored the fixtures spec test file, each test runs separately now

--- specs/fixtures_spec.rb
+++ specs/fixtures_spec.rb
@@ -2,26 +2,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper.rb')
 
 describe "Parsing JSON Fixtures" do
-  before(:all) do
-    fixtures = File.join(File.dirname(__FILE__), 'fixtures/*.json')
-    passed, failed = Dir[fixtures].partition { |f| f['pass'] }
-    @passed = passed.inject([]) { |a, f| a << [ f, File.read(f) ] }.sort
-    @failed = failed.inject([]) { |a, f| a << [ f, File.read(f) ] }.sort
-  end
   
-  it "should not be able to parse all fixtures marked as bad" do
-    @failed.each do |name, source|
-      lambda {
-        Yajl::Native.parse(StringIO.new(source))
-      }.should raise_error(Yajl::ParseError)
+  fixtures = File.join(File.dirname(__FILE__), 'fixtures/*.json')
+  passed, failed = Dir[fixtures].partition { |f| f['pass'] }
+  PASSED = passed.inject([]) { |a, f| a << [ f, File.read(f) ] }.sort
+  FAILED = failed.inject([]) { |a, f| a << [ f, File.read(f) ] }.sort
+  
+  FAILED.each do |name, source|
+    it "should not be able to parse #{File.basename(name)}" do
+        lambda {
+          Yajl::Native.parse(StringIO.new(source))
+        }.should raise_error(Yajl::ParseError)
     end
   end
   
-  it "should be able to parse all fixtures marked as good" do
-    @passed.each do |name, source|
-      lambda {
-        Yajl::Native.parse(StringIO.new(source))
-      }.should_not raise_error(Yajl::ParseError)
+  PASSED.each do |name, source|
+    it "should be able to parse #{File.basename(name)}" do
+        lambda {
+          Yajl::Native.parse(StringIO.new(source))
+        }.should_not raise_error(Yajl::ParseError)
     end
   end
 end
