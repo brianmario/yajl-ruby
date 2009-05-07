d6d3bdd93d8302f53c3d5b1337d92797526195a9
Brian Lopez <seniorlopez@gmail.com>
2009-05-06 17:18:57 -0700

    initial commit of Yajl::Deflate::StreamReader - not tested yet

---
+++ lib/yajl/deflate/stream_reader.rb
@@ -0,0 +1,26 @@
+module Yajl
+  module Deflate
+    # === Yajl::Deflate::StreamReader
+    #
+    # This is a wrapper around Zlib::Inflate, creating a #read method that adheres
+    # to the IO spec, allowing for two parameters (length, and buffer)
+    class StreamReader < ::Zlib::Inflate
+      def initialize(io)
+        @io = io
+        super(nil)
+      end
+      
+      def read(len=nil, buffer=nil)
+        buffer.gsub!(/.*/, '') unless buffer.nil?
+        buffer << inflate(io.read(len)) and return unless buffer.nil?
+        inflate(io.read(len))
+      end
+      
+      alias :finished? :eof?
+      
+      def self.parse(io)
+        Yajl::Stream.parse(new(io))
+      end
+    end
+  end
+end
