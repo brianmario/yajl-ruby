fe3f03b7671d813b062336ba765f6af05a43c571
Brian Lopez <seniorlopez@gmail.com>
2009-05-18 18:40:35 -0700

    re-enabling static compilation

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,7 +2,8 @@
 require 'mkmf'
 require 'rbconfig'
 
-# $CFLAGS << ' -Wall -static'
-# $LDFLAGS << ' -read_only_relocs suppress'
+# $CFLAGS << ' -Wall'
+$CFLAGS << ' -static'
+$LDFLAGS << ' -read_only_relocs suppress'
 
 create_makefile("yajl_ext")
