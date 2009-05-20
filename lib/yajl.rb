34858398fd93fcb2c58e6016ca6afdb46bc9313f
Brian Lopez <seniorlopez@gmail.com>
2009-05-20 07:17:07 -0700

    updating internal version

--- lib/yajl.rb
+++ lib/yajl.rb
@@ -13,7 +13,7 @@ require 'yajl_ext'
 #
 # Ruby bindings to the excellent Yajl (Yet Another JSON Parser) ANSI C library.
 module Yajl
-  VERSION = "0.4.8"
+  VERSION = "0.4.9"
   
   # == Yajl::Chunked
   #
