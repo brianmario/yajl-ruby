cd1ca0551ac93367a3e49163cd53644f8ae66b45
Brian Lopez <seniorlopez@gmail.com>
2009-05-21 17:19:08 -0700

    fixed chunked spec test to check results

--- spec/parsing/chunked_spec.rb
+++ spec/parsing/chunked_spec.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 require 'stringio'
+
 describe "Chunked parser" do
   before(:all) do
     @final = [{"abc" => 123}, {"def" => 456}]
@@ -8,7 +9,7 @@ describe "Chunked parser" do
   
   before(:each) do
     @callback = lambda { |hash|
-      hash.should == @final
+      # no-op
     }
     Yajl::Chunked.on_parse_complete = @callback
   end
@@ -18,25 +19,25 @@ describe "Chunked parser" do
   end
   
   it "should parse a single chunk" do
-    @callback.should_receive(:call)
+    @callback.should_receive(:call).with(@final)
     Yajl::Chunked << '[{"abc": 123},{"def": 456}]'
   end
   
   it "should parse a single chunk, 3 times" do
-    @callback.should_receive(:call).exactly(3).times
+    @callback.should_receive(:call).with(@final).exactly(3).times
     Yajl::Chunked << '[{"abc": 123},{"def": 456}]'
     Yajl::Chunked << '[{"abc": 123},{"def": 456}]'
     Yajl::Chunked << '[{"abc": 123},{"def": 456}]'
   end
   
   it "should parse in two chunks" do
-    @callback.should_receive(:call)
+    @callback.should_receive(:call).with(@final)
     Yajl::Chunked << '[{"abc": 123},'
     Yajl::Chunked << '{"def": 456}]'
   end
   
   it "should parse in 2 chunks, twice" do
-    @callback.should_receive(:call).exactly(2).times
+    @callback.should_receive(:call).with(@final).exactly(2).times
     Yajl::Chunked << '[{"abc": 123},'
     Yajl::Chunked << '{"def": 456}]'
     Yajl::Chunked << '[{"abc": 123},'
@@ -44,26 +45,24 @@ describe "Chunked parser" do
   end
   
   it "should parse 2 JSON strings, in 3 chunks" do
-    @callback.should_receive(:call).exactly(2).times
+    @callback.should_receive(:call).with(@final).exactly(2).times
     Yajl::Chunked << '[{"abc": 123},'
     Yajl::Chunked << '{"def": 456}][{"abc": 123},{"def":'
     Yajl::Chunked << ' 456}]'
   end
   
   it "should parse 2 JSON strings in 1 chunk" do
-    pending
-    @callback.should_receive(:call).exactly(2).times
+    @callback.should_receive(:call).with(@final).exactly(2).times
     Yajl::Chunked << '[{"abc": 123},{"def": 456}][{"abc": 123},{"def": 456}]'
   end
   
   it "should parse 2 JSON strings from an IO" do
-    pending
-    @callback.should_receive(:call).exactly(2).times
+    @callback.should_receive(:call).with(@final).exactly(2).times
     Yajl::Stream.parse(StringIO.new('[{"abc": 123},{"def": 456}][{"abc": 123},{"def": 456}]'))
   end
   
-  it "should parse an IO" do
-    @callback.should_receive(:call)
+  it "should parse a JSON string an IO and fire callback once" do
+    @callback.should_receive(:call).with(@final)
     Yajl::Stream.parse(StringIO.new('[{"abc": 123},{"def": 456}]'))
   end
 end
