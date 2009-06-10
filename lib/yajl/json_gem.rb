e4c0637ee75083b400dff9c380a37f385c272391
Brian Lopez <seniorlopez@gmail.com>
2009-06-10 09:16:14 -0700

    adding JSON::ParseError to compatability API

--- lib/yajl/json_gem.rb
+++ lib/yajl/json_gem.rb
@@ -1,34 +1,59 @@
 # encoding: UTF-8
 require 'yajl' unless defined?(Yajl::Parser)
 
+# NOTE: this is probably temporary until I can split out the JSON compat C code into it's own
+# extension that can be included when this file is.
 Yajl::Encoder.enable_json_gem_compatability
 
 module JSON
+  
+  class ParserError < Yajl::ParseError; end
+  
   def self.parse(str, opts={})
-    Yajl::Parser.parse(str, :symbolize_keys => false)
+    begin
+      Yajl::Parser.parse(str, :symbolize_keys => false)
+    rescue Yajl::ParseError => e
+      raise JSON::ParserError, e.message
+    end
   end
   
   def self.generate(obj, opts={})
-    options_map = {}
-    if opts.has_key?(:indent)
-      options_map[:pretty] = true
-      options_map[:indent] = opts[:indent]
+    begin
+      options_map = {}
+      if opts.has_key?(:indent)
+        options_map[:pretty] = true
+        options_map[:indent] = opts[:indent]
+      end
+      Yajl::Encoder.encode(obj, options_map)
+    rescue Yajl::ParseError => e
+      raise JSON::ParserError, e.message
     end
-    Yajl::Encoder.encode(obj, options_map)
   end
   
   def self.pretty_generate(obj, opts={})
-    options_map = {}
-    options_map[:pretty] = true
-    options_map[:indent] = opts[:indent] if opts.has_key?(:indent)
-    Yajl::Encoder.encode(obj, options_map)
+    begin
+      options_map = {}
+      options_map[:pretty] = true
+      options_map[:indent] = opts[:indent] if opts.has_key?(:indent)
+      Yajl::Encoder.encode(obj, options_map)
+    rescue Yajl::ParseError => e
+      raise JSON::ParserError, e.message
+    end
   end
   
   def self.load(input, *args)
-    Yajl::Parser.parse(input)
+    begin
+      Yajl::Parser.parse(input)
+    rescue Yajl::ParseError => e
+      raise JSON::ParserError, e.message
+    end
   end
   
   def self.dump(obj, io=nil, *args)
-    Yajl::Encoder.encode(obj, io)
+    begin
+      Yajl::Encoder.encode(obj, io)
+    rescue Yajl::ParseError => e
+      raise JSON::ParserError, e.message
+    end
   end
 end
