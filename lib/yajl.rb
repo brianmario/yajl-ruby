5ba022cf66c0f50215e7ac0a3cdf932921fb0d80
Brian Lopez <seniorlopez@gmail.com>
2009-08-19 22:56:39 -0700

    updating files for release

--- lib/yajl.rb
+++ lib/yajl.rb
@@ -13,7 +13,7 @@ require 'yajl_ext'
 #
 # Ruby bindings to the excellent Yajl (Yet Another JSON Parser) ANSI C library.
 module Yajl
-  VERSION = "0.5.12"
+  VERSION = "0.6.0"
   
   class Parser
     # A helper method for parse-and-forget use-cases
