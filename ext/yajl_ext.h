13c36b3644893814d62601367c1bfa5d250bfcc0
Brian Lopez <brianmario@Brians-MacPro.local>
2009-10-06 10:20:34 -0700

    add a few missing macros if they aren't defined to support older ruby versions (like 1.8.5)

--- ext/yajl_ext.h
+++ ext/yajl_ext.h
@@ -5,6 +5,20 @@
 #define READ_BUFSIZE 8092
 #define WRITE_BUFSIZE 8092
 
+// Older versions of Ruby (< 1.8.6) need these
+#ifndef RSTRING_PTR
+#define RSTRING_PTR(s) (RSTRING(s)->ptr)
+#endif
+#ifndef RSTRING_LEN
+#define RSTRING_LEN(s) (RSTRING(s)->len)
+#endif
+#ifndef RARRAY_PTR
+#define RARRAY_PTR(s) (RARRAY(s)->ptr)
+#endif
+#ifndef RARRAY_LEN
+#define RARRAY_LEN(s) (RARRAY(s)->len)
+#endif
+
 static VALUE cParseError, cEncodeError, mYajl, cParser, cEncoder;
 static ID intern_io_read, intern_eof, intern_call, intern_keys, intern_to_s, intern_to_json, intern_has_key,
           sym_allow_comments, sym_check_utf8, sym_pretty, sym_indent, sym_terminator, sym_symbolize_keys;
