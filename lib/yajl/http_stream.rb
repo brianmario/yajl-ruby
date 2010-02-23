c00a199d599f92d7fe70ea1941741c84ac0e44c8
Brian Lopez <seniorlopez@gmail.com>
2010-02-23 11:29:10 -0800

    remove trap from HttpStream code, it's not needed

--- lib/yajl/http_stream.rb
+++ lib/yajl/http_stream.rb
@@ -99,9 +99,6 @@ module Yajl
         end
 
         socket = opts.has_key?(:socket) ? opts.delete(:socket) : TCPSocket.new(uri.host, uri.port)
-        original_trap = trap("INT") {
-          return
-        }
         request = "#{method} #{uri.path}#{uri.query ? "?"+uri.query : nil} HTTP/1.1\r\n"
         request << "Host: #{uri.host}\r\n"
         request << "Authorization: Basic #{[uri.userinfo].pack('m').strip!}\r\n" unless uri.userinfo.nil?
@@ -183,7 +180,6 @@ module Yajl
           end
         end
       ensure
-        trap("INT", original_trap) if original_trap
         socket.close if !socket.nil? and !socket.closed?
       end
 
