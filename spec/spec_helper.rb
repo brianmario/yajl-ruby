ef5db1ae6649d1da0c076374bde4013c43579db9
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-05-24 06:26:15 +0800

    Reimplement the single function used by activesupport.
    Instead of adding a test dependency over activesupport, rewrite the same
    function here. The dependency over activesupport would be way exaggerated
    for just this.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,13 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 
 require 'rubygems'
 require 'yajl'
-require 'active_support/core_ext/kernel/reporting'
 require 'date'
+
+module Kernel
+  def silence_warnings
+    old_verbose, $VERBOSE = $VERBOSE, nil
+    yield
+  ensure
+    $VERBOSE = old_verbose
+  end
+end
