850ac3e6cf08aeca9199e2fb1b3f2f4f96c3a0b7
Brian Lopez <seniorlopez@gmail.com>
2010-12-19 01:59:38 -0800

    move stringio require into spec_helper

--- spec/parsing/chunked_spec.rb
+++ spec/parsing/chunked_spec.rb
@@ -1,6 +1,5 @@
 # encoding: UTF-8
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
-require 'stringio'
 
 describe "Chunked parser" do
   before(:all) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'rspec'
 require 'yajl'
 require 'date'
+require 'stringio'
 
 module Kernel
   def silence_warnings
