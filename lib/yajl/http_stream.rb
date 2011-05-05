5f73f1670c89da0d3cce2574bf97268c355e1fa0
Jon Moses <jon@burningbush.us>
2011-05-05 19:04:01 -0400

    If an instance of HttpStream is passed a socket in the options, do not ignore it.

--- lib/yajl/http_stream.rb
+++ lib/yajl/http_stream.rb
@@ -190,6 +190,8 @@ module Yajl
   private
     # Initialize socket and add it to the opts
     def initialize_socket(uri, opts = {})
+      return if opts[:socket]
+
       @socket = TCPSocket.new(uri.host, uri.port)
       opts.merge!({:socket => @socket})
       @intentional_termination = false
