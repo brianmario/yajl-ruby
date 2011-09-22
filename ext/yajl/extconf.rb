8a17376a40b646a0214effbf6ad8b3a2b8e96f0f
Brian Lopez <seniorlopez@gmail.com>
2011-09-22 15:21:57 +0300

    turn off debug symbols again

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -2,6 +2,6 @@ require 'mkmf'
 require 'rbconfig'
 
 $CFLAGS << ' -Wall -funroll-loops'
-$CFLAGS << ' -Wextra -O0 -ggdb3' #if ENV['DEBUG']
+$CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 
 create_makefile('yajl/yajl')
