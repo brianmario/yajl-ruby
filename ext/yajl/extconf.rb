00e69c27c3a7f0a4fc4c687efecd9e464679c382
Vít Ondruch <vondruch@redhat.com>
2013-01-23 15:07:30 +0100

    Remove Makefile patching.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-yajl_dir = File.expand_path('../vendor', __FILE__)
+yajl_dir = File.join($srcdir, 'vendor')
 
 $CFLAGS << " -Wall -I#{yajl_dir}"
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
@@ -8,9 +8,6 @@ $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 srcs = Dir[File.join(yajl_dir, '*.c')] + Dir[File.join(File.expand_path('..', __FILE__), '*.c')]
 $objs = srcs.map {|src| File.basename(src).gsub(/\.c$/, '.o') }
 
-create_makefile('yajl/yajl')
+$VPATH << "$(srcdir)/vendor"
 
-# now lets post-process the Makefile so we compile the vendored yajl too
-m = File.read('Makefile')
-m.gsub!(/VPATH = (.*)\n/, "VPATH = \\1#{CONFIG['PATH_SEPARATOR']}#{yajl_dir}\n")
-File.open('Makefile', 'w+') {|f| f.write(m) }
+create_makefile('yajl/yajl')
