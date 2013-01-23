b4d1c3e24d46123e039cf3f70b9448f9cd9c0af9
Vít Ondruch <vondruch@redhat.com>
2013-01-23 15:54:15 +0100

    yajl_dir is needed just for vandored yajl.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -1,6 +1,5 @@
 require 'mkmf'
 
-yajl_dir = File.join($srcdir, 'vendor')
 
 $CFLAGS << " -Wall"
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
@@ -8,6 +7,8 @@ $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 if have_library("yajl", nil, 'yajl/yajl_common.h')
   find_header("yajl_common.h", File.join($hdrdir, 'yajl'))
 else
+  yajl_dir = File.join($srcdir, 'vendor')
+
   srcs = Dir[File.join(yajl_dir, '*.c')] + Dir[File.join(File.expand_path('..', __FILE__), '*.c')]
   $objs = srcs.map {|src| File.basename(src).gsub(/\.c$/, '.o') }
 
