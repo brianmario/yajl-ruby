b17a8c15d5584949b743750fa8d25baafa42d4c2
Vít Ondruch <vondruch@redhat.com>
2013-01-23 15:42:28 +0100

    Prefer system yajl if available.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -5,11 +5,15 @@ yajl_dir = File.join($srcdir, 'vendor')
 $CFLAGS << " -Wall -I#{yajl_dir}"
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 
-srcs = Dir[File.join(yajl_dir, '*.c')] + Dir[File.join(File.expand_path('..', __FILE__), '*.c')]
-$objs = srcs.map {|src| File.basename(src).gsub(/\.c$/, '.o') }
+if have_library("yajl", nil, 'yajl/yajl_common.h')
+  find_header("yajl_common.h", File.join($hdrdir, 'yajl'))
+else
+  srcs = Dir[File.join(yajl_dir, '*.c')] + Dir[File.join(File.expand_path('..', __FILE__), '*.c')]
+  $objs = srcs.map {|src| File.basename(src).gsub(/\.c$/, '.o') }
 
-$VPATH << "$(srcdir)/vendor"
+  $VPATH << "$(srcdir)/vendor"
 
-find_header("yajl_common.h", File.join(yajl_dir, 'api'))
+  find_header("yajl_common.h", File.join(yajl_dir, 'api'))
+end
 
 create_makefile('yajl/yajl')
