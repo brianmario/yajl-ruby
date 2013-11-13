a030a7d285f5929350bb78b0cf7844a6cff9a3ae
Jesse Zhang <sbjesse@gmail.com>
2013-11-12 20:11:33 -0800

    Stop manipulating load path ourselves
    RSpec already puts lib and spec in path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/..')
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
-
 require 'rspec'
 require 'yajl'
 require 'date'
