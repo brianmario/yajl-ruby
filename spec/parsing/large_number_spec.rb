5b84c20cf1b20300b4b45f16d1b5c501a6a72198
Jesse Zhang <sbjesse@gmail.com>
2013-11-20 21:56:55 -0800

    DRY up specs

--- spec/parsing/large_number_spec.rb
+++ spec/parsing/large_number_spec.rb
@@ -2,34 +2,31 @@ require 'spec_helper'
 require 'open3'
 
 describe 'Parsing very long text' do
-  # FIXME: a better description
-  it 'parses big integer' do
-    out, err, status = Open3.capture3('ruby', stdin_data: <<-EOS)
-require "yajl"
+  shared_examples 'running script successfully' do |script|
+    it 'runs successfully' do
+      out, err, status = Open3.capture3('ruby', stdin_data: script)
+      [err, status.exitstatus].should eq(['', 0])
+    end
+  end
 
+  context 'when parsing big integers' do
+    include_examples('running script successfully', <<-EOS)
+require "yajl"
 Yajl::Parser.parse('[' + '1' * 2**23 + ']')
     EOS
-    status.exitstatus.should eq(0)
-    err.should eq('')
   end
 
-  it 'parses big float' do
-    out, err, status = Open3.capture3('ruby', stdin_data: <<-EOS)
+  context 'when parseing big floats' do
+    include_examples('running script successfully', <<-EOS)
 require "yajl"
-
 Yajl::Parser.parse('[0.' + '1' * 2**23 + ']')
     EOS
-    status.exitstatus.should eq(0)
-    err.should eq('')
   end
 
-  it 'parses long hash key with symbolize_keys option' do
-    out, err, status = Open3.capture3('ruby', stdin_data: <<-EOS)
+  context 'when parseing long hash key with symbolize_keys option' do
+    include_examples('running script successfully', <<-EOS)
 require "yajl"
-
 Yajl::Parser.parse('{"' + 'a' * 2**23 + '": 0}', symbolize_keys: true)
     EOS
-    err.should eq('')
-    status.exitstatus.should eq(0)
   end
 end
