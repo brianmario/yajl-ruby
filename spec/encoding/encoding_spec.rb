20c1195d6bc93d202007b1a9510e3a06032bdcfc
Michael Koziarski <michael@koziarski.com>
2014-05-23 12:09:46 +1200

    Test case that fails

--- spec/encoding/encoding_spec.rb
+++ spec/encoding/encoding_spec.rb
@@ -320,12 +320,22 @@ describe "Yajl JSON encoder" do
 
     Yajl::Encoder.encode(a, :sort_keys=> true).should eql(%({"a":2,"z":1}))
   end
+
   it "should not sort keys when not asked to" do
     a = {}
     a["z"] = 1
     a["a"] = 2
 
     Yajl::Encoder.encode(a).should eql(%({"z":1,"a":2}))
+  end
+
+  it "should sort keys without error when the keys are various crazy things" do
+    a = {}
+    a[:hello] = 1
+    a[0] = 2
+    a[Yajl] = 3
+
+    Yajl::Encoder.encode(a, :sort_keys=>true).should eql(%({"0":2,"Yajl":3,"hello":1}))
   end
 
 end
