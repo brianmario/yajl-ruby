68695aab1a6a4c0ca4d41e7a313529f6aefed0e9
Brett N. DiFrischia <orderthruchaos@gmail.com>
2014-10-27 12:54:18 -0500

    Specified flags are not compatible with Solaris Studio C compiler.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -1,7 +1,11 @@
 require 'mkmf'
 require 'rbconfig'
 
-$CFLAGS << ' -Wall -funroll-loops'
-$CFLAGS << ' -Werror-implicit-function-declaration -Wextra -O0 -ggdb3' if ENV['DEBUG']
+if RbConfig::CONFIG['host_os'] =~ /solaris(!?2\.1[0-2])/
+  if RbConfig::CONFIG['GCC'] != ""
+    $CFLAGS << ' -Wall -funroll-loops'
+    $CFLAGS << ' -Werror-implicit-function-declaration -Wextra -O0 -ggdb3' if ENV['DEBUG']
+  end
+end
 
 create_makefile('yajl/yajl')
