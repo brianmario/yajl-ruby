f063bcaa9b702f8f72bd17ddb00affcadd377988
Joel Low <joel@joelsplace.sg>
2015-01-25 20:47:23 +0800

    Silence compile warnings in MSVC builds.

--- ext/yajl/extconf.rb
+++ ext/yajl/extconf.rb
@@ -1,7 +1,11 @@
 require 'mkmf'
 require 'rbconfig'
 
-$CFLAGS << ' -Wall -funroll-loops'
-$CFLAGS << ' -Werror-implicit-function-declaration -Wextra -O0 -ggdb3' if ENV['DEBUG']
+if RbConfig::CONFIG['host_os'] =~ /win(32|64)/
+  $CFLAGS << ' /Od /Zi' if ENV['DEBUG']
+else
+  $CFLAGS << ' -Wall -funroll-loops'
+  $CFLAGS << ' -Werror-implicit-function-declaration -Wextra -O0 -ggdb3' if ENV['DEBUG']
+end
 
 create_makefile('yajl/yajl')
