03c44dccbeb6abb15d96eb6d1a2c4361ce646ae6
keithduncan <keith_duncan@me.com>
2017-02-25 22:52:08 +1300

    Port Array spec from json-projection

--- spec/projection/projection.rb
+++ spec/projection/projection.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 require 'stringio'
+require 'json'
 
 describe "projection" do
   it "should work" do
@@ -15,5 +16,76 @@ describe "projection" do
     projector = Yajl::Projector.new(stream)
     projection = projector.project({"name" => nil})
     expect(projection['age']).to eql(nil)
+  end
+
+  def project(schema, over: "", json: nil, stream: nil)
+    if stream.nil?
+      if json.nil?
+        json = over.to_json
+      end
+
+      stream = StringIO.new(json)
+    end
+
+    Yajl::Projector.new(stream).project(schema)
+  end
+
+  it "filters arrays" do
+    json = {
+      "users" => [
+        {
+          "name" => "keith",
+          "company" => "internet plumbing inc",
+          "department" => "janitorial",
+        },
+        {
+          "name" => "justin",
+          "company" => "big blue",
+          "department" => "programming?",
+        },
+        {
+          "name" => "alan",
+          "company" => "different colour of blue",
+          "department" => "drop bear containment",
+        }
+      ]
+    }
+
+    schema = {
+      # /users is an array of objects, each having many keys we only want name
+      "users" => {
+        "name" => nil,
+      }
+    }
+
+    expect(project(schema, over: json)).to eql({
+      "users" => [
+        { "name" => "keith" },
+        { "name" => "justin" },
+        { "name" => "alan" }
+      ]
+    })
+  end
+
+  it "filters top level arrays" do
+    json = [
+      {
+        "name" => "keith",
+        "personal detail" => "thing",
+      },
+      {
+        "name" => "cory",
+        "phone number" => "unknown",
+      }
+    ]
+
+    schema = {
+      "name" => nil,
+    }
+
+    expect(project(schema, over: json)).to eql([
+      { "name" => "keith" },
+      { "name" => "cory" },
+    ])
   end
 end
